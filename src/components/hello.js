cffffee9a285ab96a5161b929e6741441b4b71b5
Janek_Janiak <janek.janiak@gmail.com>
2021-03-20 20:55:19 +0100

    Test JSX

--- src/components/hello.js
+++ src/components/hello.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 //named export
 export const Hello = () => {
-  return <h1>Janek says hello !!!</h1>
-}
+//   return <h1>Janek says hello !!!</h1>
+// }
 
+  return React.createElement(
+    'div',
+    {id: 1, customData: 'testCustomData'},
+    React.createElement('h1', null, 'Janek says hello')
+  )
+}
 // export default hello
