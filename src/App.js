84c26663a1a4a22127894b7eb8f5ac992f43ca1e
janek <janek.janiak@gmail.com>
2021-03-20 10:28:31 +0100

    Add first functional component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Hello from './components/hello.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-      </header>
+      <Hello/>
     </div>
   );
 }
---
+++ src/components/hello.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function hello() {
+  return <h1>Janek says hello!!!</h1>
+}
+
+export default hello
