fbd46f5b455d15f6d72c58c97024ef60b65831ca
Janek_Janiak <janek.janiak@gmail.com>
2021-03-20 20:14:37 +0100

    Add class component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 // import Hello from './components/hello.js'
 import { Hello } from './components/hello.js'
 
+import HelloClass from './components/helloClass.js'
+
 function App() {
   return (
     <div className="App">
       <Hello/>
+      <HelloClass />
     </div>
   );
 }
---
+++ src/components/helloClass.js
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class HelloClass extends Component {
+  render() {
+    return <h2>Hello from Class Component</h2>
+  }
+}
+
+export default HelloClass
